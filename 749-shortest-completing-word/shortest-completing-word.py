c764037380d479bd728357984c20df356b101983
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-05-28 12:34:51 +0700

    Time: 35 ms (21.24%) | Memory: 17.9 MB (71.53%) - LeetSync

---
+++ 749-shortest-completing-word/shortest-completing-word.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        letters_in_licensePlate= [s for s in licensePlate.lower() if s.isalpha()]
+        res = []
+        for word in words:
+            temp_licensePlate = copy.deepcopy(letters_in_licensePlate)
+            for letter in word:
+                if letter in temp_licensePlate:
+                    temp_licensePlate.remove(letter)
+                
+            if len(temp_licensePlate) == 0:
+                res += [word]
+        ans = "" 
+        min_len = float('inf')
+        for x in res:
+            if len(x) < min_len:
+                min_len = len(x)
+                ans = x
+
+        return ans
+
+
+        
