9e41e43908e871e56ed79e89c367505237e944a7
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-01 11:18:02 +0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (81.12%) - LeetSync

--- 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.py
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.py
@@ -1,10 +1,6 @@
 class Solution:
     def checkEqualPartitions(self, nums: List[int], target: int) -> bool:
-        n = len(nums)
-        for r in range(1, n):
-            for subset in combinations(nums, r):
-                if prod(subset) == target:
-                    other_set = [x for x in nums if x not in subset]
-                    if other_set and prod(other_set) == target:
-                        return True
-        return False
+        product = 1
+        for each in nums:
+            product *= each
+        return product == target ** 2
