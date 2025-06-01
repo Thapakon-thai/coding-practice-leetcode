ddefe299e8355d33814c0d31d5f539b5709e7d8a
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-01 11:39:45 +0700

    Time: 12 ms (27.87%) | Memory: 17.9 MB (39.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= 0 and x < 10:
+            return True
+        if x % 10 == 0:
+            return False
+        if x < 0:
+            return False
+
+        revert_num = 0
+        while x > revert_num:
+            revert_num = revert_num * 10 + x % 10
+            x //= 10
+        
+        return x == revert_num or x == revert_num // 10
+        
