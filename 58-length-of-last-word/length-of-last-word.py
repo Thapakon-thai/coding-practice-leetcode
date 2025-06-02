27dabac31573996f86587fbf3ab33ff163020b31
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-02 16:34:33 +0700

    Time: 0 ms (100.00%) | Memory: 18 MB (17.51%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,10 +1,4 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = s.split(" ")
-        res = ""
-        for x in s[::-1]:
-            if x.isalpha():
-                res = x
-                break
-        print(s)
-        return len(res)
+        s = s.strip().split()
+        return len(s[-1])
