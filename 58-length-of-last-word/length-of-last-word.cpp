48f228b027216d9b91670977503938bd4d2a52d3
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-02 16:54:06 +0700

    Time: 0 ms (100.00%) | Memory: 9 MB (32.04%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    std::string strip(const std::string& s) {
-        auto start = std::find_if_not(s.begin(), s.end(), ::isspace);
-        auto end = std::find_if_not(s.rbegin(), s.rend(), ::isspace).base();
-        return (start < end) ? std::string(start, end) : "";
-    }
-
     int lengthOfLastWord(string s) {
-        string ss = strip(s);
-        // if (ss.size() == 1) return 1;
-        cout << ss;
         int c = 0;
-        for(int i=ss.size()-1;i >= 0; i--) {
-            if (isalpha(ss[i])) {
+        for(int i=s.size()-1;i >= 0; i--) {
+            if (s[i] != ' ') {
                 c++;
-            } else {
+            } else if (s[i] == ' ' && c != 0) {
                 return c;
             }
         }
