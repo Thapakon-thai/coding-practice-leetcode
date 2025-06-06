f667266fb375be60dffa0b241ea6d94765dec026
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-06 20:32:53 +0700

    Time: 7 ms (69.31%) | Memory: 7.8 MB (55.90%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int countTriples(int n) {
         int cnt = 0;
-        vector<vector<int>> tmp;
         for (int a = 1; a <= n; a++) {
             for(int b = 1; b <= n; b++) {
                 int t = (a*a)+(b*b);
                 int c = sqrt(t);
                 if(c <= n && c * c == t) {
-                    tmp.push_back({a,b,c});
                     cnt++;
                 }
             }
