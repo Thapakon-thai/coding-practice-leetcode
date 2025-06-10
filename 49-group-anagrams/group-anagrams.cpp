3951a26f63e1245e2304ded7fea769bbd16ef7f2
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-10 15:37:07 +0700

    Time: 71 ms (10.66%) | Memory: 34.4 MB (9.22%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -24,4 +24,4 @@ public:
         return result;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0";});
+// auto init = atexit([]() { ofstream("display_runtime.txt") << "0";}); 
