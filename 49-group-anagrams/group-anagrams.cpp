9ab489e9c351df0d2a71d521ec9cd5d1b5bb6f54
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-10 15:29:36 +0700

    Time: 79 ms (6.83%) | Memory: 34.3 MB (10.45%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> group;
+
+        for(const auto& str : strs) {
+            vector<int> count(26, 0);
+
+            for(const auto& c : str) {
+                count[c - 'a']++;
+            }
+            string key = to_string(count[0]);
+
+            for(int i = 1; i < 26; i++) {
+                key += ',' + to_string(count[i]);
+            }
+
+            group[key].push_back(str);
+        }
+        vector<vector<string>> result;
+        for(const auto& k : group) {
+            result.push_back(k.second);
+        }
+        return result;
+    }
+};
