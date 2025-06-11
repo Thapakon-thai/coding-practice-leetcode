3bbe2775145d91e5fc0c1a9758d6fd025481e523
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-11 12:10:10 +0700

    Time: 3 ms (28.70%) | Memory: 8.5 MB (82.55%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    
-
     int reverse(int x) {
-        bool minus = false;
-        long num = (long)x;
-        if (num < 0) minus = true;
-        if (minus) num = -num;  
-        long res = 0;
-        while(num > 0 ) {
-            res = res * 10 + num % 10;
-            num /= 10;
-        }
-        if(minus) res *= -1;
-        if(res < pow(-2, 31) || res > pow(2, 31) - 1) {
-            return 0;
+        int res = 0;
+        while(x != 0 ) {
+            if(res < pow(-2, 31) / 10 || res > (pow(2, 31) - 1) / 10) {
+                return 0;
+            }
+            res = (res * 10) + x % 10;
+            x /= 10;
         }
         return res;
     }
