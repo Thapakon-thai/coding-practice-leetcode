f06c69ea126cbd9d75915a0190b736e83b012a16
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2025-06-11 12:12:40 +0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (82.55%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    const int MAXINT32 = pow(2, 31) - 1;
+    const int MININT32 = pow(-2, 31);
+
     int reverse(int x) {
         int res = 0;
         while(x != 0 ) {
-            if(res < pow(-2, 31) / 10 || res > (pow(2, 31) - 1) / 10) {
+            if(res < MININT32 / 10 || res > MAXINT32 / 10) {
                 return 0;
             }
             res = (res * 10) + x % 10;
