1a55ecb04acfcfa116fe2042c000c46b7ab56583
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2026-01-23 22:56:37 +0700

    Time: 3 ms (55.46%) | Memory: 6.1 MB (51.46%) - LeetSync

--- 9-palindrome-number/palindrome-number.go
+++ 9-palindrome-number/palindrome-number.go
@@ -5,16 +5,14 @@ func isPalindrome(x int) bool {
         return true
     }
 
-    xStr := fmt.Sprintf("%d", x)
-    level := len(xStr) - 1
-    result := 0
-    tempX := x // 121
-    for i:=0; i<len(xStr); i++ {
-        result += (tempX % 10) * int(math.Pow(float64(10), float64(level)))
-        tempX /= 10
-        level--
+    num := x
+    res := 0
+    for num > 0 {
+        remainder := num % 10
+        res = (res * 10) + remainder
+        num /= 10
     }
 
     
-    return result == x
+    return res == x
 }
