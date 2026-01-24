17d7d6e1e84d7387b71af6b30bcc7169399ebdfe
Thapakon Niwongsa <thapakon_n@cmu.ac.th>
2026-01-25 00:08:57 +0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (54.51%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.go
@@ -0,0 +1,31 @@
+func minimumPairRemoval(nums []int) int {
+    count := 0
+    numsCopy := nums
+
+    for len(numsCopy) > 1 {
+        isIncreasing := true
+        minSum := 50001
+        targetIndex := -1
+        for i:=0; i<len(numsCopy)-1; i++ {
+            if nums[i] > nums[i+1] {
+                isIncreasing = false
+            } 
+
+            sum := nums[i] + nums[i+1]
+            if sum < minSum {
+                minSum = sum
+                targetIndex = i
+            }
+        }
+        if isIncreasing {
+            break
+        }
+        count++
+        numsCopy[targetIndex] = minSum
+        numsCopy = append(numsCopy[:targetIndex+1], numsCopy[targetIndex+2:]...)
+
+    }
+
+
+    return count
+}
